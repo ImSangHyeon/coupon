47aa801c68c65bf8000433ab1afcfe0732938c10
Yimga <Yimga@Yimga-PC>
2020-10-26 06:55:09 +0900

    쿠폰지급처리/지급취소처리 추가

--- src/main/java/com/kko/coupon/repository/CouponRepository.java
+++ src/main/java/com/kko/coupon/repository/CouponRepository.java
@@ -1,7 +1,6 @@
 package com.kko.coupon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.kko.coupon.model.Coupon;
 
--- src/main/java/com/kko/coupon/test/CouponCtrollerTest.java
+++ src/main/java/com/kko/coupon/test/CouponCtrollerTest.java
@@ -4,7 +4,6 @@ package com.kko.coupon.test;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
@@ -16,8 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kko.coupon.model.Coupon;
+import com.kko.coupon.model.CouponIssue;
 import com.kko.coupon.model.CouponStatCd;
 import com.kko.coupon.model.User;
+import com.kko.coupon.repository.CouponIssueRepository;
 import com.kko.coupon.repository.CouponRepository;
 import com.kko.coupon.repository.UserRepository;
 
@@ -31,6 +32,10 @@ public class CouponCtrollerTest {
 	
 	@Autowired
 	private CouponRepository couponRepository;
+	
+	@Autowired
+	private CouponIssueRepository couponIssueRepository;
+	
 	
 	final char[] psblChtrs =
 	    {'1','2','3','4','5','6','7','8','9','0'
@@ -52,8 +57,8 @@ public class CouponCtrollerTest {
 		user.setUserNm("Lion");
 		
 		user = this.userRegist(user);
+//		return user;
 		return user.getUserNm() + "닙이 가입완료되었습니다.( " + user.getUserId() + ")";
-		
 	}
 	
 	@PostMapping("user/regist")
@@ -64,6 +69,7 @@ public class CouponCtrollerTest {
 	
 	@GetMapping("user/login")
 	public String userLogin(User user) {
+		User cfmUser = userRepository.findById(user.getUserId()).get();
 		return user.getUserNm() + "닙이 로그인되었습니다.( " + user.getUserId() + ")";
 	}
 	
@@ -82,6 +88,161 @@ public class CouponCtrollerTest {
 	}
 	
 	/**
+	 * 발급된쿠폰정보조회
+	 * @param userId
+	 * @return
+	 */
+	@GetMapping("cp/getIssueInf/{userId}")
+	public String getCouponIssueInf(@PathVariable String userId) {
+		String rsltStr = "";
+		List<CouponIssue> couponIssueLst = couponIssueRepository.findAll();
+		if(couponIssueLst == null) {
+			return "발급된 쿠폰이 없습니다.";
+		}
+		if(userId == null || userId.isEmpty()) {
+			return "유저정보가 없습니다.";
+		}
+		
+		CouponIssue couponIssue = null;
+		int totCnt = couponIssueLst.size(); 
+		for (int inx =0; inx < totCnt; inx ++ ) {
+			couponIssue = couponIssueLst.get(inx);
+			if(!userId.equals(couponIssue.getUser().getUserId())) {
+				couponIssueLst.remove(inx);
+				totCnt --;
+				inx --;
+			}else {
+				if(rsltStr.isEmpty()) {
+					rsltStr = rsltStr + couponIssue.getCoupon().getCouponNo() + "/" + couponIssue.getCoupon().getCouponStatCd()+ "/" + couponIssue.getCoupon().getVrEdDt();    
+				}else {
+					rsltStr = rsltStr + "<br>" + couponIssue.getCoupon().getCouponNo() + "/" + couponIssue.getCoupon().getCouponStatCd()+ "/" + couponIssue.getCoupon().getVrEdDt();
+				}
+			}
+		}
+		
+		if(rsltStr.isEmpty()) {
+			return "발급된 쿠폰이 없습니다.";
+		}else {
+			return "지급된 쿠폰정보(" + userId +")<br>" + rsltStr;
+		}	
+	}
+	
+	@GetMapping("cp/useCoupon/{couponNo}")
+	public String useCoupon(@PathVariable String couponNo) {
+		
+		List<Coupon> couponLst = couponRepository.findAll();
+		
+		if(couponNo == null || couponNo.isEmpty()) {
+			return "쿠폰 번호를 확인해 주세요.";
+		}
+		
+		if(couponLst == null) {
+			return "사용이 불가능한 쿠폰입니다.";
+		}
+		
+		Coupon useObjCoupon = null;
+		
+		for (Coupon coupon : couponLst) {
+			if(couponNo.equals(coupon.getCouponNo())) {
+				useObjCoupon = coupon;
+				break;
+			}
+		}
+		
+		if(useObjCoupon == null ) {
+			return "사용이 불가능한 쿠폰입니다.";
+		}else if(!CouponStatCd.USE_PSBL.equals(useObjCoupon.getCouponStatCd())) {
+			return "사용이 불가능한 상태의 쿠폰입니다.";
+		}else {
+			useObjCoupon.setCouponStatCd(CouponStatCd.USE_CMPT);		//쿠폰사용 완료처리
+			couponRepository.save(useObjCoupon);
+			return couponNo + " 쿠폰이 사용되었습니다.";
+		}
+	}
+	
+	@GetMapping("cp/cancleUsedCoupon/{couponNo}")
+	public String cancleUsedCoupon(@PathVariable String couponNo) {
+		
+		List<Coupon> couponLst = couponRepository.findAll();
+		
+		if(couponNo == null || couponNo.isEmpty()) {
+			return "쿠폰 번호를 확인해 주세요.";
+		}
+		
+		if(couponLst == null) {
+			return "취소가 불가능한 쿠폰입니다.";
+		}
+		
+		Coupon useObjCoupon = null;
+		
+		for (Coupon coupon : couponLst) {
+			if(couponNo.equals(coupon.getCouponNo())) {
+				useObjCoupon = coupon;
+				break;
+			}
+		}
+		
+		if(useObjCoupon == null ) {
+			return "취소가 불가능한 쿠폰입니다.";
+		}else if(!CouponStatCd.USE_CMPT.equals(useObjCoupon.getCouponStatCd())) {
+			return "취소가 불가능한 상태의 쿠폰입니다.";
+		}else {
+			useObjCoupon.setCouponStatCd(CouponStatCd.USE_PSBL);		//쿠폰사용 사용가능처리
+			couponRepository.save(useObjCoupon);
+			return couponNo + " 쿠폰이 사용취소되었습니다.";
+		}
+	}
+	
+	/**
+	 * 유저에게 사용 가능한 쿠폰을 발행
+	 * @param userId
+	 * @return
+	 */
+	@GetMapping("cp/issue/{userId}")
+	public String regstCouponIssue(@PathVariable String userId) {
+		
+		User user = new User();
+		user.setUserId(userId);
+		
+		CouponIssue couponIssue = new CouponIssue();
+		Calendar cd = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat ( "yyyy-MM-dd");
+		String curDate = sdf.format(cd.getTime());
+		
+		couponIssue.setUser(user);
+		couponIssue.setIssueDate(curDate);
+		Coupon validcoupon = getValidCoupon();
+		if(validcoupon == null) {
+			return "발급가능한 쿠폰이 없습니다";
+		}
+		couponIssue.setCoupon(validcoupon);
+		couponIssue = couponIssueRepository.save(couponIssue);	//쿠폰발행정보생성
+		
+		couponIssue.getCoupon().setCouponStatCd(CouponStatCd.USE_PSBL);
+		couponRepository.save(couponIssue.getCoupon());		//쿠폰상태 변경 (발행완료 --> 사용가능)
+		
+		return userId + "에게 쿠폰이 지급되었습니다.(" + validcoupon.getCouponNo() + ")" ;
+	}
+	
+	/**
+	 * 발금 간능한 유효 쿠폰 조회
+	 * @return
+	 */
+	@PostMapping("cp/retv/")
+	public Coupon getValidCoupon() {
+		Coupon validcoupon = null;
+		List<Coupon> couponLst = couponRepository.findAll();
+		for (Coupon coupon : couponLst) {
+			if(CouponStatCd.CRTN_CPT.equals(coupon.getCouponStatCd())){
+				validcoupon = coupon;
+				break;
+			}
+		}
+		return validcoupon;
+	}
+	
+	
+	/**
 	 * (신규) 쿠폰 객체를 cnt 만큼 생성하여 리턴함
 	 * @param cnt
 	 * @return List<Coupon>
@@ -92,7 +253,6 @@ public class CouponCtrollerTest {
 		String[] couponNoLst = this.crtnCouponNo(totCnt);
 		
 		Calendar cd = Calendar.getInstance();
-		
 		SimpleDateFormat sdf = new SimpleDateFormat ( "yyyy-MM-dd");
 		String curDate = sdf.format(cd.getTime());
 		
